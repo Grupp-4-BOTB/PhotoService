e0090d3b0b9fa17af603c0235cdb49a4edc67492
Emil L <emillarsson99@gmail.com>
2026-05-21 09:46:40 +0200

    Created domainlayer for PhotoService

---
+++ PhotoService.Domain/Repositories/IPhotoRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhotoService.Domain.Repositories
+{
+    internal class IPhotoRepository
+    {
+    }
+}
---
+++ PhotoService.Domain/ValueObjects/OwnerId.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhotoService.Domain.ValueObjects
+{
+    internal class OwnerId
+    {
+    }
+}
