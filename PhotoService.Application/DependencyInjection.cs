6cf49a2f742b68b4607249e6f166e41bac2ee82c
Emil L <emillarsson99@gmail.com>
2026-05-26 16:19:45 +0200

    Changed name on PhotoService to PhotoAppService, Added uploadAsync method for blobContainer functionality.

---
+++ PhotoService.Application/DependencyInjection.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using PhotoService.Application.Services;
+
+namespace PhotoService.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddScoped<PhotoAppService>();
+        return services;
+    }
+}
---
+++ PhotoService.Application/Services/PhotoAppService.cs
@@ -0,0 +1,46 @@
+﻿using Azure.Storage.Blobs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using PhotoService.Domain.Entities;
+using PhotoService.Domain.Repositories;
+using PhotoService.Domain.ValueObjects;
+
+namespace PhotoService.Application.Services;
+
+public class PhotoAppService
+{
+    private readonly IPhotoRepository _repository;
+    private readonly BlobServiceClient _blobServiceClient;
+    private readonly string _containerName;
+
+    public PhotoAppService(IPhotoRepository repository, BlobServiceClient blobServiceClient, IConfiguration configuration)
+    {
+        _repository = repository;
+        _blobServiceClient = blobServiceClient;
+        _containerName = configuration["AzureBlobStorage:ImageContainerName"]
+            ?? throw new InvalidOperationException("AzureBlobStorage:ImageContainerName is missing");
+    }
+
+    public async Task<Photo> UploadAsync(OwnerId ownerId, IFormFile)
+    public async Task<Photo?> GetByIdAsync(Guid id, CancellationToken ct)
+    {
+        var photoId = new PhotoId(id);
+        return await _repository.GetByIdAsync(photoId, ct);
+    }
+
+    public async Task AddAsync(Photo photo, CancellationToken ct = default)
+    {
+        await _repository.AddAsync(photo, ct);
+    }
+
+    public async Task DeleteAsync (Guid id, CancellationToken ct = default)
+    {
+        var photoId = new PhotoId(id);
+        var photo = await _repository.GetByIdAsync(photoId, ct);
+
+        if (photo is null)
+            throw new KeyNotFoundException($"Photo with id {id} was not found.");
+
+        await _repository.DeleteAsync(photo, ct);
+    }
+}
